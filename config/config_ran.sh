608f5e8800c056c4acbd33aac738f92bbb6a4da7
Guilherme Matos <guimvmatos1@outlook.com>
2021-01-26 03:03:26 -0300

    Snort-SegmentRouting

--- config/config_ran.sh
+++ config/config_ran.sh
@@ -7,6 +7,7 @@ sudo ip link set dev eth1 up
 sudo ip -6 neigh add fc00::2 lladdr 00:15:4d:00:00:01 nud permanent dev eth1
 sudo ip -6 neigh add fc00::3 lladdr 00:15:4d:00:00:02 nud permanent dev eth1
 sudo ip -6 neigh add fc00::4 lladdr 00:15:4d:00:00:03 nud permanent dev eth1
+sudo ip -6 neigh add fc00::5 lladdr ac:1f:6b:67:06:40 nud permanent dev eth1
 sudo sysctl -w net.ipv6.conf.all.seg6_require_hmac=-1
 sudo sysctl -w net.ipv6.conf.all.accept_source_route=1
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
