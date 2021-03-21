898dfe0c6fc8850bae0a7abc0aabf89abb3f4f1f
Guilherme Matos <guimvmatos1@outlook.com>
2021-03-21 04:21:18 -0300

    update

--- config/config_upf.sh
+++ config/config_upf.sh
@@ -8,6 +8,8 @@ sudo ip -6 neigh add fc00::1 lladdr 00:15:4d:00:00:00 nud permanent dev eth1
 sudo ip -6 neigh add fc00::2 lladdr 00:15:4d:00:00:01 nud permanent dev eth1
 sudo ip -6 neigh add fc00::3 lladdr 00:15:4d:00:00:02 nud permanent dev eth1
 sudo ip -6 neigh add fc00::5 lladdr ac:1f:6b:67:06:40 nud permanent dev eth1
+sudo ip -6 neigh add fc00::8 lladdr 00:15:4d:00:00:04 nud permanent dev eth1
+sudo ip -6 neigh add fc00::9 lladdr 00:15:4d:00:00:05 nud permanent dev eth1
 sudo sysctl -w net.ipv6.conf.all.seg6_require_hmac=-1
 sudo sysctl -w net.ipv6.conf.all.accept_source_route=1
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
