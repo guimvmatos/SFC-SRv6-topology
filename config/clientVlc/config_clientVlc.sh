7f3b67e517b0e25ee2b4c62c11d3b893fd9a4346
Guilherme Matos <guimvmatos1@outlook.com>
2021-03-23 10:25:03 -0300

    fix

--- config/clientVlc/config_clientVlc.sh
+++ config/clientVlc/config_clientVlc.sh
@@ -5,17 +5,17 @@ sudo apt-get update
 sudo apt-get -y install scapy net-tools
 
 sudo ip link set dev lo up
-sudo ip -6 addr add fc00::9/64 dev eth1
-sudo ip link set dev eth1 up
-sudo ip -6 neigh add fc00::1 lladdr 00:15:4d:00:00:00 nud permanent dev eth1
-sudo ip -6 neigh add fc00::2 lladdr 00:15:4d:00:00:01 nud permanent dev eth1
-sudo ip -6 neigh add fc00::3 lladdr 00:15:4d:00:00:02 nud permanent dev eth1
-sudo ip -6 neigh add fc00::4 lladdr 00:15:4d:00:00:03 nud permanent dev eth1
-sudo ip -6 neigh add fc00::5 lladdr ac:1f:6b:67:06:40 nud permanent dev eth1
-sudo ip -6 neigh add fc00::8 lladdr 00:15:4d:00:00:04 nud permanent dev eth1
+sudo ip -6 addr add fc00::9/64 dev enp0s8
+sudo ip link set dev enp0s8 up
+sudo ip -6 neigh add fc00::1 lladdr 00:15:4d:00:00:00 nud permanent dev enp0s8
+sudo ip -6 neigh add fc00::2 lladdr 00:15:4d:00:00:01 nud permanent dev enp0s8
+sudo ip -6 neigh add fc00::3 lladdr 00:15:4d:00:00:02 nud permanent dev enp0s8
+sudo ip -6 neigh add fc00::4 lladdr 00:15:4d:00:00:03 nud permanent dev enp0s8
+sudo ip -6 neigh add fc00::5 lladdr ac:1f:6b:67:06:40 nud permanent dev enp0s8
+sudo ip -6 neigh add fc00::8 lladdr 00:15:4d:00:00:04 nud permanent dev enp0s8
 sudo sysctl -w net.ipv6.conf.all.seg6_require_hmac=-1
 sudo sysctl -w net.ipv6.conf.all.accept_source_route=1
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
-sudo sysctl -w net.ipv6.conf.eth1.seg6_require_hmac=-1
-sudo sysctl -w net.ipv6.conf.eth1.seg6_enabled=1
+sudo sysctl -w net.ipv6.conf.enp0s8.seg6_require_hmac=-1
+sudo sysctl -w net.ipv6.conf.enp0s8.seg6_enabled=1
 sudo sysctl -p 
