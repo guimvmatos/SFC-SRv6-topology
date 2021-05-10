529b7df1fc9c1e8bde6e0417b2fe28cd597cf985
Guilherme Matos <guimvmatos1@outlook.com>
2021-05-10 05:11:58 -0300

    Update config_clientNmap.sh

--- config/config_clientNmap.sh
+++ config/config_clientNmap.sh
@@ -9,8 +9,8 @@ sudo ip link set dev lo up
 sudo ip -6 addr add fc10::3/64 dev eth1
 sudo ip link set dev eth1 up
 
-sudo ip -6 neigh add fc10::1 lladdr 08:00:27:bb:bb:bb nud permanent dev enp0s9 #ran
-sudo ip -6 neigh add fc20::2 lladdr 08:00:27:dd:dd:dd nud permanent dev enp0s9 #dashServer
+sudo ip -6 neigh add fc10::1 lladdr 08:00:27:bb:bb:bb nud permanent dev eth1 #ran
+sudo ip -6 neigh add fc20::2 lladdr 08:00:27:dd:dd:dd nud permanent dev eth1 #dashServer
 sudo ip -6 route add fc20::2 via fc10::1 #rota para dashServer saindo de link secundário direto pra RAN
 
 sudo ip -6 route add fcf0:23::9009 encap seg6local action End dev eth1
